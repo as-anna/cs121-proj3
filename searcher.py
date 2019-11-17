ffd4ce6c43503de31f4c771ca60ac007c4ce0998
Anna A <patty.asprer@gmail.com>
2019-11-17 10:32:07 -0800

    Implemented very slow searcher
    Need to sort inverted index alphabetically still

--- searcher.py
+++ searcher.py
@@ -1,5 +1,39 @@
+import json
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from collections import Counter
+
+
 def main():
-    pass
+    ps = PorterStemmer()
+    scores = {}
+
+    query = input("Enter search keywords: ")
+    query_tokens = word_tokenize(query)
+
+    with open("inverted_index.txt", 'r') as f:
+        uniques = json.loads(f.read())
+
+        for token in query_tokens:
+            try:
+                for posting in uniques[ps.stem(token)]:
+                    try:
+                        scores[posting[0]] += posting[1]
+                    except:
+                        scores[posting[0]] = posting[1]
+            except:
+                continue
+
+    top = Counter(scores).most_common(5)
+
+    with open("page_index.txt", 'r') as f:
+        pages = json.loads(f.read())
+
+        print("Top results for " + query + ":")
+        index = 1
+        for key, result in top:
+            print(str(index) + ": " + pages[str(key)])
+            index += 1
 
 
 if __name__ == '__main__':
