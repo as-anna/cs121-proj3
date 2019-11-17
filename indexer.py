3e5e2ab512f1b3257bc8552abaabf370cb10a5bf
Anna A <patty.asprer@gmail.com>
2019-11-17 10:30:29 -0800

    Ditched Posting class and changed to list

--- indexer.py
+++ indexer.py
@@ -6,34 +6,37 @@ import json
 from collections import defaultdict
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
-from Posting import Posting
+from nltk.corpus import stopwords
 import math
 
 page_index = {}
+stop_words = set(stopwords.words('english'))
+ps = PorterStemmer()
 
 
 def main():
     uniques = scorer()
-    print(len(uniques))
 
     with open("page_index.txt", 'w') as pages:
-        pages.write(json.dumps(page_index))
+        for chunk in json.JSONEncoder().iterencode(page_index):
+            pages.write(chunk)
 
     with open("inverted_index.txt", 'w') as our_index:
-        our_index.write(json.dumps(uniques))
+        for chunk in json.JSONEncoder().iterencode(uniques):
+            our_index.write(chunk)
 
 
 def scorer():
     uniques, doc_count = indexer()
+    print(len(uniques))
     print("doc_count: " + str(doc_count))
 
     for token in uniques:
         df = len(uniques[token])
         idf = math.log(doc_count/df)
         for posting in uniques[token]:
-            # tf = posting.tf_idf
-            this_tf_idf = (1 + math.log(posting.tf_idf)) * idf
-            posting.__init__(posting.doc_id, round(this_tf_idf, 2), posting.fields)
+            this_tf_idf = (1 + math.log(posting[1])) * idf
+            posting[1] = round(this_tf_idf, 2)
 
     return uniques
 
@@ -41,7 +44,6 @@ def scorer():
 def indexer():
 
     index = 0
-    ps = PorterStemmer()
     uniques = defaultdict(list)
 
     # argv[1] should be something like DEV/** or DEV/[folder]/**
@@ -49,46 +51,54 @@ def indexer():
 
         if os.path.isfile(page):
             # Open the json file and read
-            print(page)
-            with open(page, 'r') as f:
-                page_json = f.read()
-
+            try:
                 # Take the URL of the page and add it to the page index
-                page_url = json.loads(page_json)['url']
+                page_url = json.load(open(page))['url']
                 page_index[index] = page_url
 
                 # Take the content from the json
-                page_content = json.loads(page_json)['content']
+                page_content = json.load(open(page))['content']
                 t_tokens = page_content.strip().split()
                 tokens = [t for t in t_tokens if re.match(r'[^\W\d]*$', t)]
                 f_tokens = ' '.join(tokens)
 
                 nltk_tokens = word_tokenize(f_tokens)
-                # print(nltk_tokens)
 
                 for token in nltk_tokens:
+                    if token.lower() in stop_words:
+                        continue
+
                     token = token.lower()
                     token = ps.stem(token)
 
                     # If doc id already in a posting under that token add 1 to score
                     for posting in uniques[token]:
-                        if index is posting.doc_id:
-                            posting.__init__(posting.doc_id, posting.tf_idf + 1, posting.fields)
+                        if index is posting[0]:
+                            posting[1] += 1
                             break
                     else:
-                        uniques[token].append(Posting(index, 1, []))
+                        uniques[token].append([index, 1, []])
 
                 print(page_url)
 
                 index += 1
-
+                print(index)
             # TODO: Remove this thing once done with testing 1 page
             # break
 
+            except KeyboardInterrupt:
+
+                with open("page_index.txt", 'w') as pages:
+                    for chunk in json.JSONEncoder().iterencode(page_index):
+                        pages.write(chunk)
+
+                with open("inverted_index.txt", 'w') as our_index:
+                    for chunk in json.JSONEncoder().iterencode(uniques):
+                        our_index.write(chunk)
+
+                quit()
+
     return uniques, index
-
-
-# def scorer(token):
 
 
 if __name__ == '__main__':
