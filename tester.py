8b8b628123067f620fa3aed6d41db8606a67389b
Anna A <patty.asprer@gmail.com>
2019-11-29 18:08:12 -0800

    Added a tester file for response times

--- tester.py
+++ tester.py
@@ -1,15 +1,18 @@
 import time
+import sys
+import json
 
 
 def main():
-    with open("full_index.txt", 'r') as fp:
+    with open(sys.argv[1], 'r') as fp:
         start = time.time()
         i = 0
-        line = fp.readline()
-        while line:
+        while True:
             line = fp.readline()
+            if not line:
+                break
+            line_dict = json.loads(line)
             i += 1
-
         print("Took: ", time.time() - start)
         print("Tokens: ", i)
 
